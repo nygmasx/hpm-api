87851e8652423a608db1b46f7b51c9ce97f2d117
nygmasx <imraneeslk@gmail.com>
2024-10-13 14:30:53 +0200

    working

--- app/Models/TemperatureChangement.php
+++ app/Models/TemperatureChangement.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class TemperatureChangement extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'operation_type',
+        'additional_informations',
+        'user_id',
+        'start_date',
+        'start_temperature',
+        'end_temperature',
+        'end_date',
+        'corrective_action'
+    ];
 }
