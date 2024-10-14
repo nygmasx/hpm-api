b3b21270335fd2c6af558b61cad10cb1474b43d4
nygmasx <imraneeslk@gmail.com>
2024-10-14 07:02:46 +0200

    working

--- database/migrations/2024_09_15_045514_create_temperature_changements_table.php
+++ database/migrations/2024_09_15_045514_create_temperature_changements_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
             $table->string('operation_type');
             $table->text('additional_informations')->nullable();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->dateTime('start_date');
+            $table->string('start_date');
             $table->string('start_temperature');
             $table->boolean('is_finished')->default(false);
-            $table->dateTime('end_date')->nullable();
+            $table->string('end_date')->nullable();
             $table->string('end_temperature')->nullable();
             $table->string('corrective_action')->nullable();
             $table->timestamps();
