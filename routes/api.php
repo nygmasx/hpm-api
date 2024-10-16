55fa5210ca3477eb34fdf39933f35635aa1cd374
nygmasx <imraneeslk@gmail.com>
2024-10-16 20:20:56 +0200

    ajd

--- routes/api.php
+++ routes/api.php
@@ -523,7 +523,10 @@ Route::middleware('auth:sanctum')->post('/tcp/new', function (Request $request) 
 });
 
 Route::get('user/{user}/temperature-changement', function (User $user) {
-    $receptions = $user->temperatureChangements()->with('products')->get();
+    $receptions = $user->temperatureChangements()
+        ->with('products')
+        ->where('is_finished', false)
+        ->get();
 
     return response()->json($receptions);
 });
