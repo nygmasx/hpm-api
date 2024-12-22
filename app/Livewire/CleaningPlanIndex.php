32b989573ba585091ba7be53c5bb9bb330b39f09
Imrane Sallak <imrane@pc-588.home>
2024-12-22 14:00:14 +0100

    Cleaning ZOne BO done

--- app/Livewire/CleaningPlanIndex.php
+++ app/Livewire/CleaningPlanIndex.php
@@ -8,34 +8,51 @@ use App\Models\CleaningTask;
 use App\Models\CleaningZone;
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CleaningPlanIndex extends Component
 {
+    use WithPagination;
+
     public string $search = '';
     public bool $editMode = false;
 
     public function render()
     {
         $cleaningTasks = CleaningTask::with('users')
-        ->where('title', 'like', '%' . $this->search . '%')
-        ->orWhereHas('users', function ($query) {
-            $query->where('name', 'like', '%' . $this->search . '%');
-        })
-        ->paginate(10);
+            ->where('title', 'like', '%' . $this->search . '%')
+            ->orWhereHas('users', function ($query) {
+                $query->where('name', 'like', '%' . $this->search . '%');
+            })
+            ->paginate(10);
 
-        $lastCleaningTasks = CleaningTask::orderBy('created_at', 'desc')->paginate(3);
-        $cleaningStations = CleaningStation::orderBy('created_at', 'desc')->paginate(3);
+        $lastCleaningTasks = CleaningTask::join('users_cleaning_tasks', 'cleaning_tasks.id', '=', 'users_cleaning_tasks.cleaning_task_id')
+            ->join('users', 'users.id', '=', 'users_cleaning_tasks.user_id')
+            ->select(
+                'cleaning_tasks.*',
+                'users.name as completed_by',
+                'users_cleaning_tasks.updated_at as completed_at'
+            )
+            ->where('users_cleaning_tasks.is_completed', true)
+            ->orderBy('users_cleaning_tasks.updated_at', 'desc')
+            ->limit(3)
+            ->get();
+        $cleaningStations = CleaningStation::with('cleaningZone')
+            ->orderBy('created_at', 'desc')
+            ->limit(3)
+            ->get();
 
-    $headers = [
-        ['key' => 'id', 'label' => '#', 'class' => 'text-lg'],
-        ['key' => 'title', 'label' => 'Titre', 'class' => 'text-lg'],
-        ['key' => 'estimated_time', 'label' => 'Temps estimé (minutes)', 'class' => 'text-lg'],
-        ['key' => 'products', 'label' => 'Produit(s)', 'class' => 'text-lg'],
-        ['key' => 'verification_type', 'label' => 'Type de vérification', 'class' => 'text-lg'],
-        ['key' => 'frequency', 'label' => 'Fréquence', 'class' => 'text-lg'],
-    ];
+        $headers = [
+            ['key' => 'id', 'label' => '#', 'class' => 'text-lg'],
+            ['key' => 'title', 'label' => 'Titre', 'class' => 'text-lg'],
+            ['key' => 'estimated_time', 'label' => 'Temps estimé (minutes)', 'class' => 'text-lg'],
+            ['key' => 'products', 'label' => 'Produit(s)', 'class' => 'text-lg'],
+            ['key' => 'verification_type', 'label' => 'Type de vérification', 'class' => 'text-lg'],
+            ['key' => 'frequency', 'label' => 'Fréquence', 'class' => 'text-lg'],
+        ];
 
-        return view('livewire.cleaning-plan-index', compact(['cleaningTasks', 'lastCleaningTasks', 'cleaningStations'], 'headers'))
+        return view('livewire.cleaning-plan-index',
+            compact('cleaningTasks', 'lastCleaningTasks', 'cleaningStations', 'headers'))
             ->layout('layouts.app');
     }
 
@@ -51,5 +68,4 @@ class CleaningPlanIndex extends Component
         $this->editMode = true;
         $this->postModal = true;
     }
-
 }
--- resources/views/livewire/cleaning-plan-index.blade.php
+++ resources/views/livewire/cleaning-plan-index.blade.php
@@ -8,15 +8,16 @@
             <x-mary-button icon="o-plus" class="bg-emerald-700 text-white border-0" link="/cleaning-task/new"/>
         </x-slot:actions>
     </x-mary-header>
+
     <div class="mb-4 flex justify-between gap-4">
         <x-mary-card title="Postes de Nettoyage" class="bg-white text-black w-1/2" separator>
             <div class="flex justify-between">
                 <div class="space-y-4 w-full">
-                    @foreach($cleaningStations as $cleaningStation )
+                    @foreach($cleaningStations as $cleaningStation)
                         <div class="flex justify-between w-full">
                             <div class="w-2/3">
-                                <p class="font-semibold text-xl">{{$cleaningStation->name }}</p>
-                                <p class="text-gray-400 text-sm truncate">{{$cleaningStation->cleaningZone->name }}</p>
+                                <p class="font-semibold text-xl">{{ $cleaningStation->name }}</p>
+                                <p class="text-gray-400 text-sm truncate">{{ $cleaningStation->cleaningZone->name }}</p>
                             </div>
                         </div>
                     @endforeach
@@ -28,19 +29,30 @@
                 </a>
             </x-slot:menu>
         </x-mary-card>
+
         <x-mary-card title="Dernières Tâches Effectuées" class="bg-white text-black w-1/2" separator>
             <div class="flex justify-between">
                 <div class="space-y-4 w-full">
-                    @foreach($lastCleaningTasks as $lastCleaningTask )
+                    @foreach($lastCleaningTasks as $lastCleaningTask)
                         <div class="flex justify-between w-full">
                             <div class="w-2/3">
-                                <p class="font-semibold text-xl">{{$lastCleaningTask->title }}</p>
-                                <p class="text-gray-400 text-sm truncate">{{$lastCleaningTask->verification_type }}</p>
+                                <p class="font-semibold text-xl">{{ $lastCleaningTask->title }}</p>
+                                <div class="flex flex-col gap-1">
+                                    <p class="text-gray-400 text-sm truncate">
+                                        {{ $lastCleaningTask->verification_type }}
+                                    </p>
+                                    <p class="text-gray-400 text-sm">
+                                        Complété par: {{ $lastCleaningTask->completed_by }}
+                                    </p>
+                                    <p class="text-gray-400 text-sm">
+                                        {{ \Carbon\Carbon::parse($lastCleaningTask->completed_at)->diffForHumans() }}
+                                    </p>
+                                </div>
                             </div>
-                            <div class="">
+                            <div>
                                 <x-mary-badge
                                     class="bg-emerald-600 text-white font-bold border-none"
-                                    value="{{$lastCleaningTask->estimated_time}} min"
+                                    value="{{ $lastCleaningTask->estimated_time }} min"
                                 />
                             </div>
                         </div>
@@ -49,6 +61,7 @@
             </div>
         </x-mary-card>
     </div>
+
     <x-mary-table :headers="$headers" :rows="$cleaningTasks" class="bg-emerald-700 text-white mt-4">
         @scope('actions', $cleaningTask)
         <div class="flex flex-row gap-2">
